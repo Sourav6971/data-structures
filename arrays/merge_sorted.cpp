5bc55b82f3c4021d636b09dc5836097291f39888
sourav <mailsourav014@gmail.com>
2025-04-11 00:46:34 +0530

    two-pointer in merged_sorted

--- arrays/merge_sorted.cpp
+++ arrays/merge_sorted.cpp
@@ -16,13 +16,34 @@ public:
         sort(nums1.begin(), nums1.end());
         return nums1;
     }
+    vector<int> pointerApproach(vector<int> nums1, int m, vector<int> nums2, int n)
+    { // we can use a two pointer approach as the arrays are sorted.
+
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int i = m + n - 1;
+        while (p2 >= 0)
+        {
+            if (p1 >= 0 && nums1[p1] > nums2[p2])
+            {
+                nums1[i--] = nums1[p1--];
+            }
+            else
+            {
+                nums1[i--] = nums2[p2--];
+            }
+        }
+        return nums1;
+    }
 };
 int main()
 {
     Solution sol;
     vector<int> nums1 = {1, 2, 3, 0, 0, 0};
     vector<int> nums2 = {2, 5, 6};
-    vector<int> out = sol.returnSorted(nums1, 3, nums2, 3);
+    // vector<int> out = sol.returnSorted(nums1, 3, nums2, 3);
+    vector<int> out = sol.pointerApproach(nums1, 3, nums2, 3);
+
     for (int n : out)
     {
         cout << n << " ";
