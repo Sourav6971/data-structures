0785eabcbfec1169b436c635a9557a0774deedab
sourav <mailsourav014@gmail.com>
2025-04-17 15:19:08 +0530

    solved substring without repitition

---
+++ strings/sub_without_rep.cpp
@@ -0,0 +1,43 @@
+// Solution to the problem calculate the length of the longest substring wihhout repitition on leetcode
+
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxLength(string s)
+    {
+        map<char, int> check;
+        int max_length = 0;
+        int curr_length = 0;
+        for (int i = 0; i < s.length(); i++)
+        {
+            char c = s[i];
+            if (check.count(c))
+            {
+                curr_length = 0;
+                i = check[c];
+                check.clear();
+            }
+            else
+            {
+                curr_length++;
+                check[c] = i;
+            }
+            max_length = max(max_length, curr_length);
+        }
+
+        return max_length;
+    }
+};
+
+int main()
+{
+    string s = "dvdf";
+    Solution sol;
+    int answer = sol.maxLength(s);
+    cout << answer << "";
+}
