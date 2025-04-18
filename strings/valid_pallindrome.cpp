8668829cf0b5eb5c1e7be327861d151b98c238ff
sourav <mailsourav014@gmail.com>
2025-04-18 11:06:45 +0530

    solved valid pallindrome

---
+++ strings/valid_pallindrome.cpp
@@ -0,0 +1,39 @@
+// Solution to the leetcode problem to check if a sentence is pallindrome or not,
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isPallen(string s)
+    {
+        string s1 = "";
+        for (char c : s)
+        {
+            if (isalpha(c) || isdigit(c))
+                s1 += c;
+        }
+        transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
+        int slow = 0;
+        int fast = s1.length() - 1;
+        while (slow < fast)
+        {
+            if (s1[slow] != s1[fast])
+                return false;
+            slow++;
+            fast--;
+        }
+        return true;
+    }
+};
+
+int main()
+{
+    string s = "A man, a plan, a canal: Panama";
+    Solution sol;
+    string out = sol.isPallen(s) ? "True" : "False";
+    cout << out << endl;
+}
